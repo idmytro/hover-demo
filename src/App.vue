86a282ac68f8d0fcf8303b71ff5c72cd9cba8762
Dmytro Cisar <dmytro.cisar@gmail.com>
2022-02-05 00:13:52 +0200

    done

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,54 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button
+      class="h-3rem w-20rem transition-colors"
+      :class="status.cls"
+      @mouseover="hover"
+      @mousedown="press"
+      @click="click"
+      @mouseleave="leave"
+    >{{ status.label }}</button>
+    <section class="mt-2rem">
+      <h1 class="font-bold">LEGEND:</h1>
+      <ul>
+        <li class="h-2rem p-20px inline-flex justify-center items-center" :class="statuses.initial.cls">initial</li>
+        <li class="h-2rem p-20px inline-flex justify-center items-center" :class="statuses.hovered.cls">hovered</li>
+        <li class="h-2rem p-20px inline-flex justify-center items-center" :class="statuses.pressed.cls">pressed</li>
+        <li class="h-2rem p-20px inline-flex justify-center items-center" :class="statuses.clicked.cls">clicked</li>
+      </ul>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+const statuses = {
+  initial: { cls: 'bg-green-300', label: 'HOVER OR CLICK ME' },
+  hovered: { cls: 'bg-yellow-300', label: 'HOVERED' },
+  pressed: { cls: 'bg-red-300', label: 'PRESSED' },
+  clicked: { cls: 'bg-blue-300', label: 'CLICKED' },
+};
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
+  data () {
+    return {
+      statuses,
+      status: { ...statuses.initial },
+    };
+  },
+  methods: {
+    press () {
+      this.status = { ...statuses.pressed };
+    },
+    click () {
+      this.status = { ...statuses.clicked };
+    },
+    hover () {
+      this.status = { ...statuses.hovered };
+    },
+    leave () {
+      this.status = { ...statuses.initial };
+    },
   },
 };
 </script>
