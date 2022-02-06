0b3b871563d6d901bfa215875e47db2c00d7c20d
Dmytro Cisar <dmytro.cisar@gmail.com>
2022-02-07 00:19:32 +0200

    add legend

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
-  <div id="app">
+  <div id="app" class="select-none">
     <h1 class="mt-2rem mb-1rem">1. Default behavior</h1>
     <div class="flex gap-10px justify-center">
-      <button class="w-2/5 h-3rem select-none rounded bg-green-300 !active:bg-blue-300 hover:bg-yellow-300">Button</button>
-      <button class="w-2/5 h-3rem select-none rounded bg-green-300 !active:bg-blue-300 hover:bg-yellow-300">Button</button>
+      <button class="w-2/5 h-3rem rounded bg-green-300 !active:bg-blue-300 hover:bg-yellow-300">Button</button>
+      <button class="w-2/5 h-3rem rounded bg-green-300 !active:bg-blue-300 hover:bg-yellow-300">Button</button>
     </div>
     <h1 class="mt-2rem mb-1rem">2. Disabled hover on mobile</h1>
     <div class="flex gap-10px justify-center">
-      <button class="w-2/5 h-3rem select-none rounded bg-green-300 !active:bg-blue-300 can-hover:hover:bg-yellow-300">Button</button>
-      <button class="w-2/5 h-3rem select-none rounded bg-green-300 !active:bg-blue-300 can-hover:hover:bg-yellow-300">Button</button>
+      <button class="w-2/5 h-3rem rounded bg-green-300 !active:bg-blue-300 can-hover:hover:bg-yellow-300">Button</button>
+      <button class="w-2/5 h-3rem rounded bg-green-300 !active:bg-blue-300 can-hover:hover:bg-yellow-300">Button</button>
+    </div>
+    <h1 class="mt-2rem mb-1rem">Legend:</h1>
+    <div class="flex justify-center">
+      <p class="w-25% p-2 bg-green-300">Initial</p>
+      <p class="w-25% p-2 bg-yellow-300">Hovered</p>
+      <p class="w-25% p-2 bg-blue-300">Pressed</p>
     </div>
   </div>
 </template>
